7508b49fb9d79689b54c6df4a9e9c3a4d577bd7c
kammitama5 <krystal.maughan@gmail.com>
2016-07-18 15:10:52 -0700

    2nd commit

--- myscript.js
+++ myscript.js
@@ -5,10 +5,16 @@ var pageNode = document.getElementById('header');
 pageNode.style.color = 'red';
 
 var pageNode1 = document.getElementById('footer');
-pageNode1.style.color = 'green';
+pageNode1.style.color = '#003B00';
+
+var resultsBox1 = document.getElementById('resultsBox');
+resultsBox1.style.color = 'black';
 
 var image = document.getElementById('image').src = "JumpingOrca.jpg";
 
 //=============================================================================
 
+var pageNode2 = document.getElementsByTagName('body')[0];
+pageNode2.style.backgroundColor = '#9370DB';
+pageNode2.style.paddingTop = '10px';
 
