26c7c3a521db06d0222211bda5df679ce465cc1e
kammitama5 <krystal.maughan@gmail.com>
2016-07-18 14:54:35 -0700

    7_18_16_OrcaWhaleWebsite

---
+++ myscript.js
@@ -0,0 +1,14 @@
+/*Add your own code here!*/
+//alert('hello!');
+
+var pageNode = document.getElementById('header');
+pageNode.style.color = 'red';
+
+var pageNode1 = document.getElementById('footer');
+pageNode1.style.color = 'green';
+
+var image = document.getElementById('image').src = "JumpingOrca.jpg";
+
+//=============================================================================
+
+
